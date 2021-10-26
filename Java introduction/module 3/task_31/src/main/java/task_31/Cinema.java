ca3897ff56c939485981adaa9d046b7e7e8863ac
Michał Pycia <pycia.michal@gmail.com>
2021-10-26 19:15:12 +0200

    task 3.1 solved

--- Java introduction/module 3/task_31/src/main/java/task_31/Cinema.java
+++ Java introduction/module 3/task_31/src/main/java/task_31/Cinema.java
@@ -3,7 +3,5 @@ package task_31;
 public class Cinema {
     public static void main(String[] args) {
 
-        Seats seat = new Seats(1, 3);
-
     }
 }
--- Java introduction/module 3/task_31/src/main/java/task_31/Seats.java
+++ Java introduction/module 3/task_31/src/main/java/task_31/Seats.java
@@ -1,7 +1,13 @@
 package task_31;
 
 public class Seats {
+     protected int number;
+     protected int row;
+     boolean isFree;
 
-     int number;
-     int row;
+     public Seats(int number, int row, boolean isFree) {
+          this.number = number;
+          this.row = row;
+          this.isFree = isFree;
+     }
 }
