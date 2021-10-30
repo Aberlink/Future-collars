001871466bd3d253da5d1a4aa16d51390e741470
Michał Pycia <pycia.michal@gmail.com>
2021-10-30 22:32:37 +0200

    task 3.4 solved

---
+++ Java introduction/module 3/task_34/src/main/java/task_34/Main.java
@@ -0,0 +1,10 @@
+package task_34;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(MinutesToSecondsConverter.minutesToSeconds(4.5));
+
+    }
+}
---
+++ Java introduction/module 3/task_34/src/main/java/task_34/MinutesToSecondsConverter.java
@@ -0,0 +1,9 @@
+package task_34;
+
+public class MinutesToSecondsConverter {
+
+    public static double minutesToSeconds (double minutes) {
+        return minutes * 60;
+    }
+
+}
