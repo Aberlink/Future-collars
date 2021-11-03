7eca21e0ec95a46291216675a4edd851837243fa
Aberlink <pycia.michal@gmail.com>
2021-11-03 19:06:22 +0100

    discount calculation improved

--- Java introduction/module 4/task_46/src/main/java/BillCalculator.java
+++ Java introduction/module 4/task_46/src/main/java/BillCalculator.java
@@ -6,13 +6,9 @@ public class BillCalculator {
         return (float) (billValue + serviceFee);
     }
 
-    float calculate(double billValue, float serviceFee, double discount) {
-        if (discount == 0) {
-            return (float) (billValue + serviceFee);
-        }
-        else {
-            return (float) (billValue * discount) + serviceFee;
-        }
+    float calculate(double billValue, float serviceFee, double discountInPercents) {
+
+        return (float) (billValue * (1 - discountInPercents / 100)) + serviceFee;
     }
 
     float calculate(double billValue, float serviceFee, short packing) {
