a7c28d4e80a2aa7fd4c27ee3c92661d7d465abc0
Aberlink <pycia.michal@gmail.com>
2021-11-30 00:45:25 +0100

    task 8.4

--- Java introduction/module 8/module8/src/main/java/task_84/Crew.java
+++ Java introduction/module 8/module8/src/main/java/task_84/Crew.java
@@ -4,48 +4,44 @@ import java.util.*;
 
 public class Crew {
 
-    static List<Person> crew = new ArrayList<>();
+    private static final List<Person> crew = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        fillCrew();
+        fillCrew("Jan", "Kowalski",1985,180,80);
+        fillCrew("Adam","Kwiatkowski",1994,174,84);
+        fillCrew("Tomasz","Nowak",1958,192,101);
+        fillCrew("Anna","Polak",1999,162,54);
+
         sortByHeight();
         sortByWeight();
         sortByAge();
+        System.out.println(crew);
     }
 
     public static void sortByHeight(){
-        Comparator<Person> compareByHeight = (o1, o2) -> o1.height.compareTo(o2.height);
+        Comparator<Person> compareByHeight = Comparator.comparing(o -> o.height);
         crew.sort(compareByHeight);
     }
 
     public static void sortByWeight(){
-        Comparator<Person> compareByWeight = (o1, o2) -> o1.weight.compareTo(o2.weight);
+        Comparator<Person> compareByWeight = Comparator.comparing(o -> o.weight);
         crew.sort(compareByWeight.reversed());
     }
+
     public static void sortByAge(){
-        Comparator<Person> compareByAge = (o1, o2) -> o1.birthYear.compareTo(o2.birthYear);
+        Comparator<Person> compareByAge = Comparator.comparing(o -> o.birthYear);
         crew.sort(compareByAge.reversed());
     }
-
-
 
     private static List<Person> getCrew(Person person) {
         crew.add(person);
         return crew;
     }
-    private static void fillCrew(){
-        Person plumber = new Person("Jan", "Kowalski",1985,180,80);
-        getCrew(plumber);
-        Person carpenter = new Person("Adam","Kwiatkowski",1994,174,84);
-        getCrew(carpenter);
-        Person tailor = new Person("Tomasz","Nowak",1958,192,101);
-        getCrew(tailor);
-        Person waitress = new Person("Anna","Polak",1999,162,54);
-        getCrew(waitress);
+
+    static void fillCrew(String name, String surname, int birthYear, int height, double weight) {
+        Person p = new Person(name, surname, birthYear,height,weight);
+        getCrew(p);
         sortByAge();
     }
-
-
-
 }
