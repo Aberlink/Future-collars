9b7188f21bca489be48d9aace3b83c2fa539bbc1
Aberlink <pycia.michal@gmail.com>
2021-11-29 15:51:06 +0100

    task 8.2 solved

--- Java introduction/module 8/module8/src/main/java/task_82/ListManager.java
+++ Java introduction/module 8/module8/src/main/java/task_82/ListManager.java
@@ -1,8 +1,11 @@
 package task_82;
 
+import java.util.Arrays;
+
 public class ListManager implements OwnList{
 
-    static int[] listToManage = new int[10];
+
+    int[] listToManage = new int[10];
 
     public ListManager() {
     }
@@ -14,32 +17,60 @@ public class ListManager implements OwnList{
 
     @Override
     public boolean isEmpty() {
-        boolean temporaryIsNotEmpty = false;
-        for (int arguments : listToManage) {
-            if (arguments != Integer.parseInt(null)){
-                temporaryIsNotEmpty = true;
+        boolean check = true;
+        for(int i = 0; i < listToManage.length; i++) {
+            if(listToManage[i] != 0){
+                check = false;
+                break;
             }
         }
-        return temporaryIsNotEmpty;
+        return check;
     }
 
     @Override
     public void add(Integer element) {
-
+      int[] temporary = Arrays.copyOf(listToManage,listToManage.length + 1);
+      temporary[temporary.length - 1] = element;
+      listToManage = temporary;
     }
 
     @Override
     public Integer get(int i) {
-        return null;
+        if(0 <= i && i < listToManage.length) {
+            return listToManage[i];
+        }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     @Override
     public void add(int index, Integer element) {
-
+        if(0 <= index && index < listToManage.length + 1) {
+            int[] temporary = new int[listToManage.length + 1];
+            System.arraycopy(listToManage, 0, temporary, 0, index);
+            temporary[index] = element;
+            System.arraycopy(listToManage, index, temporary, index + 1, listToManage.length - index);
+            listToManage = temporary;
+        }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 
     @Override
     public Integer remove(int index) {
-        return null;
+
+        if(0 <= index && index < listToManage.length) {
+            int[] temporary = new int[listToManage.length - 1];
+            System.arraycopy(listToManage, 0, temporary, 0, index);
+            System.arraycopy(listToManage, index + 1, temporary, index, temporary.length - index);
+            listToManage = temporary;
+            return index;
+        }
+        else {
+            throw new IndexOutOfBoundsException();
+        }
+
     }
 }
--- Java introduction/module 8/module8/src/main/java/task_82/MyOwnList.java
+++ Java introduction/module 8/module8/src/main/java/task_82/MyOwnList.java
@@ -1,9 +1,23 @@
 package task_82;
 
+import java.util.Arrays;
+
 public class MyOwnList {
 
     public static void main(String[] args) {
+
+
         ListManager listManager = new ListManager();
+
+        listManager.add(2);
+        listManager.add(0,5);
+        listManager.add(12,5);
+
+
+        System.out.println(listManager.size());
+        System.out.println(listManager.get(12));
         System.out.println(listManager.isEmpty());
+        System.out.println(listManager.remove(12));
+        System.out.println(Arrays.toString(listManager.listToManage));
     }
 }
