9fcade56f5f70b375a1b8e98cd9e0339ff8568e1
Aberlink <pycia.michal@gmail.com>
2021-11-30 19:32:15 +0100

    task 8.4 to TreeSet

--- Java introduction/module 8/module8/src/main/java/task_84/Crew.java
+++ Java introduction/module 8/module8/src/main/java/task_84/Crew.java
@@ -4,24 +4,21 @@ import java.util.*;
 
 public class Crew {
 
-    private static Set<Person> crew = new TreeSet<>();
+
 
     public static void main(String[] args) {
 
-        fillCrew("Jan", "Kowalski",1985,180,80);
-        fillCrew("Adam","Kwiatkowski",1994,174,84);
-        fillCrew("Tomasz","Nowak",1958,192,101);
-        fillCrew("Anna","Polak",1999,162,54);
+        Person person = new Person();
 
-        TreeSet<Person> byHeight = new TreeSet<>(new PersonByHeightComparator().reversed());
-        byHeight.addAll(crew);
-        crew = byHeight;
+        person.fillCrew("Jan", "Kowalski",1985,180,80);
+        person.fillCrew("Adam","Kwiatkowski",1994,174,84);
+        person.fillCrew("Tomasz","Nowak",2001,142,101);
+        person.fillCrew("Anna","Polak",1999,162,54);
 
+        System.out.println(person.crew);
+        person.sortByHeight();
+        System.out.println(person.crew);
+        person.sortByWeight();
+        System.out.println(person.crew);
     }
-
-    static void fillCrew(String name, String surname, int birthYear, int height, double weight) {
-        crew.add(new Person(name, surname, birthYear, height, weight));
-    }
-
-
 }
--- Java introduction/module 8/module8/src/main/java/task_84/Person.java
+++ Java introduction/module 8/module8/src/main/java/task_84/Person.java
@@ -12,6 +12,8 @@ public class Person  implements Comparable<Person>{
     Integer height;
     Double weight;
 
+    Set<Person> crew = new TreeSet<>();
+
     public Person(String name, String surname, int birthYear, int height, double weight) {
         this.name = name;
         this.surname = surname;
@@ -20,8 +22,27 @@ public class Person  implements Comparable<Person>{
         this.weight = weight;
     }
 
+    public Person() {
+    }
+
     @Override
     public int compareTo(Person o) {
         return Integer.compare(this.birthYear, o.birthYear);
+    }
+
+    void fillCrew(String name, String surname, int birthYear, int height, double weight) {
+        crew.add(new Person(name, surname, birthYear, height, weight));
+    }
+
+    void sortByHeight() {
+        TreeSet<Person> byHeight = new TreeSet<>(new PersonByHeightComparator().reversed());
+        byHeight.addAll(crew);
+        crew = byHeight;
+    }
+
+    void sortByWeight() {
+        TreeSet<Person> byHeight = new TreeSet<>(new PersonByWeightComparator().reversed());
+        byHeight.addAll(crew);
+        crew = byHeight;
     }
 }
