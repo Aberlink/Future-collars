478601e0355014da22576f8c5f6b344ceffb6924
Aberlink <pycia.michal@gmail.com>
2021-12-03 20:48:45 +0100

    task 9.1

---
+++ Java introduction/module 9/module_9/src/main/java/task_91/MyOwnMap.java
@@ -0,0 +1,28 @@
+package task_91;
+
+public class MyOwnMap implements OwnMap{
+    @Override
+    public boolean put(String key, String value) {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(String key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public String remove(String key) {
+        return null;
+    }
+
+    @Override
+    public String get(String key) {
+        return null;
+    }
+}
